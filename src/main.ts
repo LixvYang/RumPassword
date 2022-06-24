b37c21c46fe15322e5e632a375757db5fae4eefc
lixin yang <2690688423@qq.com>
2022-06-24 17:05:28 +0800

    style: update config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { registerApp } from './global'
+import { globalRegister } from './global'
 
 const app = createApp(App)
-registerApp(app)
 app.use(router)
 app.use(store)
-
+app.use(globalRegister)
 app.mount('#app')
---
+++ src/service/request/config.ts
@@ -0,0 +1,12 @@
+let BASE_URL = ''
+const TIME_OUT = 10000
+
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'https://localhost:64147/api/v1/'
+} else if (process.env.NODE_ENV === 'production') {
+  BASE_URL = 'https://localhost:64147/api/v1/'
+} else {
+  BASE_URL = '/test'
+}
+
+export { BASE_URL, TIME_OUT }
